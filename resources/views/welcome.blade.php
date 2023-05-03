0cc6c901cf1aff1ae96a95b8e76000e2199d4b98
PavelAsk <pavelask@mail.ru>
2023-05-03 03:35:59 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,17 @@
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
                 <Link href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
-                Dashboard
+                Личный кабинет
                 </Link>
             @else
                 <Link href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
-                Log in
+                Вход
                 </Link>
 
                 @if (Route::has('register'))
                     <Link href="{{ route('register') }}"
                           class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
-                    Register
+                    Регистрация
                     </Link>
                 @endif
             @endauth
